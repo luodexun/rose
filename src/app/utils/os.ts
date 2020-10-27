f9772f9ecbcfc3b5447ca53642a5905e14c057fe
luodexun <lll279906908@outlook.com>
2020-10-27 12:30:28 +0800

    Update os.ts

--- src/app/utils/os.ts
+++ src/app/utils/os.ts
@@ -1,12 +1,13 @@
 export const localhost = ()=>{
-  let interfaces = require('os').networkInterfaces();
-  for (let devName in interfaces) {
-    let iface = interfaces[devName];
-    for (let index in iface) {
-      let alias = iface[index];
-      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
-        return alias.address;
-      }
-    }
-  }
+  return '192.168.0.201'
+  // let interfaces = require('os').networkInterfaces();
+  // for (let devName in interfaces) {
+  //   let iface = interfaces[devName];
+  //   for (let index in iface) {
+  //     let alias = iface[index];
+  //     if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+  //       return alias.address;
+  //     }
+  //   }
+  // }
 };
